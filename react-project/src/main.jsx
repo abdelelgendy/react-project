6be61a9eb6ebc37127d7a0373945e716652658d9
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-08-04 23:50:44 -0400

    creating react static page

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import reactLogo from './assets/react.svg'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {createRoot} from 'react-dom/client'
+createRoot(document.getElementById("root")).render(
+    <App/>
 )
