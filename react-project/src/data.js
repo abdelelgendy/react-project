37b1f06f51c430f75102edc7ff40c44ab0f35024
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-08-09 02:30:34 -0400

    adding extra data

--- react-project/src/data.js
+++ react-project/src/data.js
@@ -35,4 +35,40 @@ export default [
         dates: "01 Oct, 2021 - 18 Nov, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
+    {
+        id: 4,
+        img: {
+            src: "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+            alt: "Banff National Park"
+        },
+        title: "Banff National Park",
+        country: "Canada",
+        googleMapsLink: "https://maps.app.goo.gl/2vQw8n5TQw8n5TQw8",
+        dates: "15 Jul, 2021 - 22 Jul, 2021",
+        text: "Banff National Park is Canada's oldest national park, located in the Rocky Mountains. It is known for its stunning mountain landscapes, turquoise lakes, and diverse wildlife."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://images.unsplash.com/photo-1465156799763-2c087c332922",
+            alt: "Machu Picchu"
+        },
+        title: "Machu Picchu",
+        country: "Peru",
+        googleMapsLink: "https://maps.app.goo.gl/3vQw8n5TQw8n5TQw8",
+        dates: "10 Sep, 2021 - 20 Sep, 2021",
+        text: "Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, renowned for its archaeological significance and breathtaking views."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429",
+            alt: "Santorini"
+        },
+        title: "Santorini",
+        country: "Greece",
+        googleMapsLink: "https://maps.app.goo.gl/4vQw8n5TQw8n5TQw8",
+        dates: "05 Aug, 2021 - 12 Aug, 2021",
+        text: "Santorini is a Greek island in the Aegean Sea, famous for its white-washed houses, blue-domed churches, and stunning sunsets."
+    },
 ]
